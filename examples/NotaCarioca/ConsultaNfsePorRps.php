e59b1f43fb66525f6a8076669810eda5fc8d6d4f
Pedro Casado <pdr33n@gmail.com>
2020-02-12 18:51:43 -0300

    add example files

---
+++ examples/NotaCarioca/ConsultaNfsePorRps.php
@@ -0,0 +1,109 @@
+<?php
+
+require __DIR__.'/../../vendor/autoload.php';
+
+use NFSePHP\NotaCarioca\ConsultarNfsePorRpsFactory;
+use NFSePHP\NotaCarioca\SoapHandler;
+
+$rps = [
+    'IdentificacaoRps' => [
+        'Numero' => 1,
+        'Serie' => 'A',
+        'Tipo' => 1,
+    ],
+    'Prestador' => [
+        'Cnpj' => '12218427000172',
+        'InscricaoMunicipal' => '02247211',
+    ],
+];
+
+$env = 'dev'; // dev or prod
+$notaCariocaConsulta = new ConsultarNfsePorRpsFactory($rps, $env);
+
+$soapHandler = new SoapHandler(['cert_path' => '/path/to/valid/cert.pfx', 'cert_pass' => 'certpassword']);
+
+// Send SOAP xml
+$response = $soapHandler->send($notaCariocaConsulta);
+
+if ($soapHandler->isSuccess($response)) {
+    $nfs = $notaCariocaConsulta->formatSuccessResponse($response);
+
+    var_dump($nfs);
+} else {
+    $errors = $soapHandler->getErrors($response);
+
+    var_dump($errors);
+}
+
+/* Response
+
+array (size=1)
+  'nfse' =>
+    array (size=14)
+      'Numero' => string '1' (length=2)
+      'CodigoVerificacao' => string 'AMXG-UHBL' (length=9)
+      'DataEmissao' => string '2020-02-12T14:31:39' (length=19)
+      'IdentificacaoRps' =>
+        array (size=3)
+          'Numero' => string '1' (length=3)
+          'Serie' => string 'A' (length=1)
+          'Tipo' => string '1' (length=1)
+      'DataEmissaoRps' => string '2020-02-12' (length=10)
+      'NaturezaOperacao' => string '1' (length=1)
+      'OptanteSimplesNacional' => string '2' (length=1)
+      'IncentivadorCultural' => string '2' (length=1)
+      'Competencia' => string '2020-02-12T00:00:00' (length=19)
+      'Servico' =>
+        array (size=5)
+          'Valores' =>
+            array (size=6)
+              'ValorServicos' => string '228.6' (length=5)
+              'IssRetido' => string '2' (length=1)
+              'ValorIss' => string '11.43' (length=5)
+              'BaseCalculo' => string '228.6' (length=5)
+              'Aliquota' => string '0.05' (length=4)
+              'ValorLiquidoNfse' => string '228.6' (length=5)
+          'ItemListaServico' => string '1002' (length=4)
+          'CodigoTributacaoMunicipio' => string '100203' (length=6)
+          'Discriminacao' => string 'Pedido #1111 - Itens: #123 , #124' (length=65)
+          'CodigoMunicipio' => string '3304557' (length=7)
+      'ValorCredito' => string '1.14' (length=4)
+      'PrestadorServico' =>
+        array (size=4)
+          'IdentificacaoPrestador' =>
+            array (size=2)
+              'Cnpj' => string '12218427000172' (length=14)
+              'InscricaoMunicipal' => string '02247211' (length=7)
+          'RazaoSocial' => string 'RAZAO SOCIAL' (length=48)
+          'Endereco' =>
+            array (size=7)
+              'Endereco' => string 'RUA VISC DE PIRAJA' (length=39)
+              'Numero' => string '1' (length=3)
+              'Complemento' => string 'SAL 1' (length=15)
+              'Bairro' => string 'IPANEMA' (length=7)
+              'CodigoMunicipio' => string '3304557' (length=7)
+              'Uf' => string 'RJ' (length=2)
+              'Cep' => string '11111111' (length=8)
+          'Contato' => string '' (length=0)
+      'TomadorServico' =>
+        array (size=3)
+          'IdentificacaoTomador' =>
+            array (size=1)
+              'CpfCnpj' =>
+                array (size=1)
+                  'Cpf' => string '11111111111' (length=11)
+          'RazaoSocial' => string 'Pedro L' (length=19)
+          'Endereco' =>
+            array (size=7)
+              'Endereco' => string 'Rua x, ap 1' (length=35)
+              'Numero' => string '1' (length=3)
+              'Complemento' => string 'ap 1' (length=7)
+              'Bairro' => string 'Caxias' (length=7)
+              'CodigoMunicipio' => string '3304557' (length=7)
+              'Uf' => string 'RJ' (length=2)
+              'Cep' => string '11111111' (length=8)
+      'OrgaoGerador' =>
+        array (size=2)
+          'CodigoMunicipio' => string '3304557' (length=7)
+          'Uf' => string 'RJ' (length=2)
+*/
---
+++ examples/NotaCarioca/GerarNfse.php
@@ -0,0 +1,126 @@
+<?php
+
+require __DIR__.'/../../vendor/autoload.php';
+
+use NFSePHP\NotaCarioca\GerarNfseNotaCariocaFactory;
+use NFSePHP\NotaCarioca\SoapHandler;
+
+$rps = [
+    'IdentificacaoRps' => [
+        'Numero' => 119,
+        'Serie' => 'A',
+        'Tipo' => 1,
+    ],
+    'DataEmissao' => date('Y-m-d').'T'.date('H:i:s'),
+    'NaturezaOperacao' => 1,
+    // 1 – Tributação no município
+    // 2 - Tributação fora do município
+    // 3 - Isenção
+    // 4 - Imune
+    // 5 – Exigibilidade suspensa por decisão judicial
+    // 6 – Exigibilidade suspensa por procedimento administrativo
+
+    'RegimeEspecialTributacao' => 3, // optional
+    // 1 – Microempresa municipal
+    // 2 - Estimativa
+    // 3 – Sociedade de profissionais
+    // 4 – Cooperativa
+    // 5 – MEI – Simples Nacional
+    // 6 – ME EPP – Simples Nacional
+
+    'OptanteSimplesNacional' => 2, // 1 - Sim 2 - Não
+    'IncentivadorCultural' => 2, // 1 - Sim 2 - Não
+    'Status' => 1, // 1 – Normal  2 – Cancelado
+
+    'Prestador' => [
+        'Cnpj' => '111111',
+        'InscricaoMunicipal' => '11111',
+    ],
+
+    'Tomador' => [
+        'IdentificacaoTomador' => [
+            'CpfCnpj' => [
+                'Cpf' => '111',
+                // 'Cnpj' => '111',
+            ],
+        ],
+        'Razao Social' => 'Fulano de tal',
+        'Endereco' => [
+            'Endereco' => 'Rua 1111',
+            'Numero' => '1',
+            'Complemento' => 'ap 1',
+            'Bairro' => '1',
+            'CodigoMunicipio' => 1111111,
+            'Uf' => 'RJ',
+            'Cep' => 11111111,
+        ],
+    ],
+
+    'Servico' => [
+        'ItemListaServico' => '1002', // First 4 digits - https://notacarioca.rio.gov.br/files/leis/Resolucao_2617_2010_anexo2.pdf
+        'CodigoTributacaoMunicipio' => '100203', // 6 digits - https://notacarioca.rio.gov.br/files/leis/Resolucao_2617_2010_anexo2.pdf
+        'Discriminacao' => 'Pedido #1111 - Itens: #123 , #124',
+        'CodigoMunicipio' => 1111111,
+        'Valores' => [
+            'ValorServicos' => 228.6,
+            'ValorDeducoes' => 10.0, // optional
+            'ValorPis' => 10.0, // optional
+            'ValorCofins' => 10.0, // optional
+            'ValorInss' => 10.0, // optional
+            'ValorIr' => 10.0, // optional
+            'ValorCsll' => 10.0, // optional
+            'IssRetido' => 2, // 1 para ISS Retido - 2 para ISS não Retido,
+            'ValorIss' => 10.0, // optional
+            'OutrasRetencoes' => 10.0, // optional
+            'Aliquota' => 5, // optional
+            'DescontoIncondicionado' => 10.0, // optional
+            'DescontoCondicionado' => 10.0, // optional
+        ],
+    ],
+
+    'IntermediarioServico' => [ // optional
+        'RazaoSocial' => 'Fulano de tal',
+        'CpfCnpj' => [
+            'Cnpj' => '11111',
+            // 'Cpf' => '1111',
+        ],
+        'InscricaoMunicipal' => '11111', // optional
+    ],
+
+    'ConstrucaoCivil' => [ // optional
+        'CodigoObra' => '111',
+        'Art' => '111',
+    ],
+];
+
+$env = 'dev'; // dev - prod
+$notaCarioca = new GerarNfseNotaCariocaFactory($rps, $env);
+
+$soapHandler = new SoapHandler(['cert_path' => '/path/to/valid/cert.pfx', 'cert_pass' => 'certpassword']);
+
+// Send SOAP xml
+$response = $soapHandler->send($notaCarioca);
+
+if ($soapHandler->isSuccess($response)) {
+    // save db
+    $nfs = $notaCarioca->formatSuccessResponse($response);
+    var_dump($nfs);
+} else {
+    var_dump($soapHandler->getErrors($response));
+}
+
+/* Response
+
+array (size=1)
+  'nfse' =>
+    array (size=5)
+      'Numero' => string '1' (length=2)
+      'CodigoVerificacao' => string 'AMXA-UHBL' (length=9)
+      'DataEmissao' => string '2020-02-12T14:31:39' (length=19)
+      'IdentificacaoRps' =>
+        array (size=3)
+          'Numero' => string '1' (length=3)
+          'Serie' => string 'A' (length=1)
+          'Tipo' => string '1' (length=1)
+      'DataEmissaoRps' => string '2020-02-12' (length=10)
+*/
